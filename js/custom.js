c4fc96149bf5ad16216b6e996158ae409464ecc3
anrbl <133613567+anrbl@users.noreply.github.com>
2023-07-26 18:15:56 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,48 @@
 $(function () {
-    $('.__fullpage').fullpage({
-        scrollHorizontally: true,
-    });
 
+    $('.header .user_area i').on('click', function (e) {
+        $('.user_nav').toggleClass('on')
+        $('.user_area i').toggleClass('on')
+    })
+
+    $('.__fullpage').fullpage({
+        anchors: ['main', 'sec1', 'sec2', 'sec3', 'footer'],
+        scrollingSpeed: 700,
+        scrollHorizontally: true,
+
+        navigation: false,
+        css3: false,
+        responsiveWidth: 768,
+        scrollOverflow: true,
+        easing: 'easeInOutCubic',
+        easingCss3: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+
+        afterLoad: function (lnk, idx) {
+            $('.side_nav li').eq(idx - 1).addClass('on').siblings().removeClass('on');
+        },
+        onLeave: function (idx, nidx, dir) {
+            console.log(idx, nidx, dir);
+            if (nidx == '2') {
+                $('.header').addClass('on')
+                $('.side_nav').removeClass('on')
+            } else if (nidx == '1') {
+                $('.header').removeClass('on')
+                $('.side_nav').addClass('on')
+            } else if (nidx == '4') {
+                $('.header').addClass('on')
+                $('.side_nav').removeClass('on')
+            } else if (nidx == '5') {
+                $('.header').addClass('on')
+                $('.side_nav').addClass('on')
+            } else {
+                $('.header').removeClass('on')
+                $('.side_nav').removeClass('on')
+            };
+
+            $('.side_nav li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
+
+        },
+    });
 
     const MainSlide = new Swiper('.main_slide', {
         loop: true,
@@ -11,23 +51,15 @@ $(function () {
             disableOnInteraction: false,
         },
         grabCursor: true,
-        effect: "creative",
-        creativeEffect: {
-            prev: {
-                translate: ["-20%", 0, -1],
-            },
-            next: {
-                translate: ["100%", 0, 0],
-            },
-        },
+        effect: "fade",
         navigation: {
             nextEl: '.arrows .right',
             prevEl: '.arrows .left',
-        },
+        }
     });
 
 
-    const NewsSlide = new Swiper('.snews_slide', {
+    const NewsSlide = new Swiper('.news_slide', {
         loop: true,
         spaceBetween: 30,
         effect: "fade",
@@ -36,4 +68,19 @@ $(function () {
             prevEl: '.news_arrow .nleft',
         },
     })
+
+
+    const MotorSlide = new Swiper('.motor_slide', {
+        slidesPerView: 3,
+        grid: {
+            rows: 2,
+        },
+        spaceBetween: 30,
+        pagination: {
+            el: ".main_motorcycle .dots",
+            clickable: true,
+        },
+    })
+
+
 });
